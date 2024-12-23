3d50f0bed06a7e3798939f4195ab2c9a655cf46a
divya0319 <divygupta0319@gmail.com>
2024-12-23 10:14:56 +0530

    Added CustomAuthenticationProvider to DefaultSecurityConfig

--- Oauth-authorization-server/src/main/java/com/fastturtle/oauthserver/configs/DefaultSecurityConfig.java
+++ Oauth-authorization-server/src/main/java/com/fastturtle/oauthserver/configs/DefaultSecurityConfig.java
@@ -1,13 +1,22 @@
 package com.fastturtle.oauthserver.configs;
 
+import com.fastturtle.oauthserver.services.CustomAuthenticationProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
+@Configuration
 public class DefaultSecurityConfig {
+
+    @Autowired
+    private CustomAuthenticationProvider customAuthenticationProvider;
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -17,6 +26,12 @@ public class DefaultSecurityConfig {
                 .formLogin(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Autowired
+    public void bindAuthenticationProvider(AuthenticationManagerBuilder authenticationManagerBuilder) {
+        authenticationManagerBuilder
+                .authenticationProvider(customAuthenticationProvider);
     }
 
 }
